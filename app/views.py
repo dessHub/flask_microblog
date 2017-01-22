ae5549b154aa232b3f56e44af0b488d75e86b872
dessHub <deskip95@gmail.com>
2017-01-22 21:19:52 +0300

    incoparate templates into the api

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {"nickname":"dess"}
+    post = [{'author':{'nickname':'creig'},'body':'its a beutifull day'},{'author':{'nickname':'ben'},'body':'Flask is awesom'}]
+    return render_template('index.html',title='Home',user=user,post=post)
